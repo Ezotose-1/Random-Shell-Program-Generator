53fd80a7dba5abd92b6da1b72ba9a3a5cdb2c7ad
Pierre <pierre.blaess@gmail.fr>
2021-12-18 23:54:35 +0100

    [ADD] Correct sexy prompt, test in examples dir

---
+++ examples/script1.sh
@@ -0,0 +1,30 @@
+var2=4
+if ls ; then 
+	ls 
+fi 
+
+if 0; then 
+	cat Yaaaa 
+	pwd 
+	echo Code Lyoko $var2 j'ai plus d'idee 
+else
+	cd lorem 
+	cat 42sh 
+	pwd 
+fi 
+
+cat $var2 
+cd DE DANA echo informaticien $var1 DE DANA 
+
+until do 
+	pwd 
+done 
+
+cat moulette ls 
+
+cat jojo 
+var1=4
+if [ 1 -le 4 ]; cat $var1 ; [ 2 -ne 5 ]; then 
+	cat $var1 
+fi 
+
